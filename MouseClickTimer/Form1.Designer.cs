890b5fd1dca7456fc8f514c1cd1241ee3addeccd
Charlie Gadd <gaddc11@gmail.com>
2022-01-20 00:48:22 +1100

    Add project files.

---
+++ MouseClickTimer/Form1.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace MouseClickTimer
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(12, 4);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(220, 42);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(244, 54);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "RTD Timer";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Label label1;
+    }
+}
---
+++ MouseClickTimer/Form1.cs
@@ -0,0 +1,84 @@
+using Gma.System.MouseKeyHook;
+
+using System.Diagnostics;
+
+namespace MouseClickTimer
+{
+    public partial class Form1 : Form
+    {
+        private IKeyboardMouseEvents events;
+        private DateTime lastClick = DateTime.Now;
+        private bool closing;
+        private bool firstClick = true;
+
+        public Form1()
+        {
+            Shown += (s, e) => Task.Run(UpdateLabel);
+            FormClosing += (s, e) => closing = true;
+
+            events = Hook.GlobalEvents();
+            events.MouseClick += Events_MouseClick;
+
+            InitializeComponent();
+
+            label1.MouseClick += (s, e) =>
+            {
+                if (e.Button == MouseButtons.Right)
+                {
+                    firstClick = !firstClick;
+                }
+            };
+        }
+
+        private void Events_MouseClick(object? sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.XButton1 && ((DateTime.Now - lastClick) >= TimeSpan.FromSeconds(60) || firstClick))
+            {
+                lastClick = DateTime.Now;
+                firstClick = false;
+            }
+        }
+
+        private void UpdateLabel()
+        {
+            while (!closing)
+            {
+                try
+                {
+                    Thread.Sleep(1);
+
+                    TimeSpan timeSinceLastClick = DateTime.Now - lastClick;
+
+                    Invoke(() => label1.Text = timeSinceLastClick.ToString());
+
+                    if (firstClick && label1.ForeColor != Color.Orange)
+                    {
+                        label1.ForeColor = Color.Orange;
+                    }
+                    else if (!firstClick && timeSinceLastClick < TimeSpan.FromSeconds(60) && label1.ForeColor != Color.Red)
+                    {
+                        label1.ForeColor = Color.Red;
+                    }
+                    else if (!firstClick && timeSinceLastClick >= TimeSpan.FromSeconds(60) && label1.ForeColor != Color.Green)
+                    {
+                        label1.ForeColor = Color.Green;
+                    }
+                }
+                catch (Exception e)
+                {
+                    if (!closing)
+                    {
+                        if (File.Exists(@".\error.txt"))
+                        {
+                            File.Delete(@".\error.txt");
+                        }
+
+                        File.WriteAllText(@".\error.txt", e.ToString());
+
+                        MessageBox.Show(e.Message, "Error logged to error.txt");
+                    }
+                }
+            }
+        }
+    }
+}
